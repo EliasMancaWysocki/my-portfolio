8da1e47000e2ea91090396a3897655636de13af9
Elias Lautaro Manca Wysocki <eliasmanca20@gmail.com>
2025-07-15 20:51:27 -0300

    añadido un poco de info personal
    No se qué chota voy a poner tanto en data

--- src/pages/DataPage.tsx
+++ src/pages/DataPage.tsx
@@ -1,7 +1,44 @@
+import { useTranslation } from 'react-i18next';
+
 export default function DataPage() {
-    return(
-        <div className='flex justify-center items-center'>
-            data
+
+    const { t } = useTranslation();
+
+    return (
+        <div className='w-1/2 p-5'>
+            
+            <div className='flex'>
+                <div className='w-[10ch]'>{t("name")} </div>
+                <div>Elías Lautaro Manca Wysocki</div>
+            </div>
+
+            <div className='flex'>
+                <div className='w-[10ch]'>{t("phone")} </div>
+                <a 
+                    href="https://wa.me/+543513976336" 
+                    target="_blank"
+                    className='hover:underline'
+                >+54 351 3976336</a>
+            </div>
+
+            <div className='flex'>
+                <div className='w-[10ch]'>{t("email")} </div>
+                <a
+                    href="mailto:eliasmanca20@gmail.com"
+                    target='_blank'
+                    className='hover:underline'
+                >eliasmanca20@gmail.com</a>
+            </div>
+            
+            <div className='flex'>
+                <div className='w-[10ch]'>{t("github")} </div>
+                <a
+                    href='https://github.com/EliasMancaWysocki'
+                    target='_blank'
+                    className='hover:underline'
+                >github.com/EliasMancaWysocki</a>
+            </div>
+
         </div>
     )
 }
